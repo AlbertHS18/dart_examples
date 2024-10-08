ca3cbcc127875c417252dc887bcb46550996f77d
Albert <albert182219@gmail.com>
2024-10-08 19:41:45 -0400

    Commit2

--- main.dart
+++ main.dart
@@ -1,23 +1,36 @@
 // Interpolación de Strings
 
+// void main() {
+//   String nombre = "Albert";
+//   int edad = 23;
+
+  
+//   print("Hola, me llamo $nombre y tengo $edad años.");
+
+
+//   print("El próximo año tendré ${edad + 1} años.");
+
+
+//   print("Mi nombre en mayúsculas es ${nombre.toUpperCase()}.");
+
+  
+//   String descripcion = """
+//   Mi nombre es $nombre.
+//   Este año tengo $edad años.
+//   """;
+//   print(descripcion);
+// }
+
+// Operadores de valores nulos 
+
 void main() {
-  String nombre = "Albert";
-  int edad = 23;
+  String? nombre; // Variable que puede ser nula
 
-  
-  print("Hola, me llamo $nombre y tengo $edad años.");
+  print(nombre ?? "Nombre no asignado");
+
+  // ??= asigna un valor solo si la variable es nula
+  nombre ??= "Juan";
+  print(nombre); // Ahora imprime "Juan"
 
 
-  print("El próximo año tendré ${edad + 1} años.");
-
-
-  print("Mi nombre en mayúsculas es ${nombre.toUpperCase()}.");
-
-  
-  String descripcion = """
-  Mi nombre es $nombre.
-  Este año tengo $edad años.
-  """;
-  print(descripcion);
 }
-
