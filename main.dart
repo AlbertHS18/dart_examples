d9a4e11a1d144ad3017dc532d68ab5c74dd14f2e
Albert <albert182219@gmail.com>
2024-10-08 19:37:24 -0400

    Commit1

--- main.dart
+++ main.dart
@@ -1,3 +1,23 @@
+// Interpolación de Strings
+
 void main() {
-  print('Hello, Dart!');
+  String nombre = "Albert";
+  int edad = 23;
+
+  
+  print("Hola, me llamo $nombre y tengo $edad años.");
+
+
+  print("El próximo año tendré ${edad + 1} años.");
+
+
+  print("Mi nombre en mayúsculas es ${nombre.toUpperCase()}.");
+
+  
+  String descripcion = """
+  Mi nombre es $nombre.
+  Este año tengo $edad años.
+  """;
+  print(descripcion);
 }
+
